e3952ea327a3c7506ed77088494ef238c1d92c4b
sawada14 <matthewsandfrey@gmail.com>
2021-06-24 20:29:18 -0700

    New  Car Select up and good
    - can select which car you want via arrows
    - can choose color via arrows too
    - looks pretty good

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -29,6 +29,8 @@ public class MainMenu : MonoBehaviour
     //cars
     public GameObject car;
     public GameObject car3DObject;
+    private int currCar = 1;
+    private int currColor = 1;
     private bool hasIntroPlayer = false;
     private bool playIntro = false;
     private bool isPlaying = false;
@@ -97,6 +99,7 @@ public class MainMenu : MonoBehaviour
         level.SetActive(true);
         level3DObject.SetActive(true);
         car.SetActive(false);
+        car3DObject.SetActive(false);
     }
     public void EnterLevelSelect()
     {
@@ -220,13 +223,222 @@ public class MainMenu : MonoBehaviour
                 break;
         }
     }
+    public void LeftCar()
+    {
+        currCar--;
+        if (currCar <= 0)
+        {
+            currCar = 4;
+        }
+        DisplayCar(currCar);
+        DisplayCarColor(currCar, currColor);
+    }
+    public void RightCar()
+    {
+        currCar++;
+        if (currCar > 4)
+        {
+            currCar = 1;
+        }
+        DisplayCar(currCar);
+        DisplayCarColor(currCar, currColor);
+    }
+    void DisplayCar(int car)
+    {
+        switch (car)
+        {
+            case 1://racer
+                car3DObject.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>("Models/Starter_Car_2");
+                FindObjectOfType<InGameController>().racerType = 1;
+                break;
+            case 2://agile
+                car3DObject.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>("Models/Agile_Car_1");
+                FindObjectOfType<InGameController>().racerType = 2;
+                break;
+            case 3://heavy
+                car3DObject.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>("Models/Heavy_Car_1");
+                FindObjectOfType<InGameController>().racerType = 3;
+                break;
+            case 4://speed
+                car3DObject.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>("Models/Speed_Car_1");
+                FindObjectOfType<InGameController>().racerType = 4;
+                break;
+            default:
+                car3DObject.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>("Models/Starter_Car_2");
+                FindObjectOfType<InGameController>().racerType = 1;
+                break;
+        }
+    }
+    public void UpCar()
+    {
+        currColor--;
+        if (currColor <= 0)
+        {
+            currColor = 5;
+        }
+        DisplayCarColor(currCar, currColor);
+    }
+    public void DownCar()
+    {
+        currColor++;
+        if (currColor > 5)
+        {
+            currColor = 1;
+        }
+        DisplayCarColor(currCar, currColor);
+    }
+    void DisplayCarColor(int carType, int color)
+    {
+        switch (carType)
+        {
+            case 1:
+                Car1Color(color);
+                break;
+            case 2:
+                Car2Color(color);
+                break;
+            case 3:
+                Car3Color(color);
+                break;
+            case 4:
+                Car4Color(color);
+                break;
+            default:
+                Car1Color(color);
+                break;
+        }
+    }
+    //racer colors
+    void Car1Color(int color)
+    {
+        switch (color)
+        {
+            case 1://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_1_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+            case 2://gold
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_1_Material_Gold");
+                FindObjectOfType<InGameController>().playerCarColor = "yellow";
+                break;
+            case 3://orange
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_1_Material_Orange");
+                FindObjectOfType<InGameController>().playerCarColor = "orange";
+                break;
+            case 4://pink
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_1_Material_Pink");
+                FindObjectOfType<InGameController>().playerCarColor = "magenta";
+                break;
+            case 5://red
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_1_Material_Red");
+                FindObjectOfType<InGameController>().playerCarColor = "red";
+                break;
+            default://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_1_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+        }
+    }
+    //agile colors
+    void Car2Color(int color)
+    {
+        switch (color)
+        {
+            case 1://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_2_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+            case 2://gold
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_2_Material_Gold");
+                FindObjectOfType<InGameController>().playerCarColor = "yellow";
+                break;
+            case 3://orange
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_2_Material_Orange");
+                FindObjectOfType<InGameController>().playerCarColor = "orange";
+                break;
+            case 4://pink
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_2_Material_Pink");
+                FindObjectOfType<InGameController>().playerCarColor = "magenta";
+                break;
+            case 5://red
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_2_Material_Red");
+                FindObjectOfType<InGameController>().playerCarColor = "red";
+                break;
+            default://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_2_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+        }
+    }
+    //heavy colors
+    void Car3Color(int color)
+    {
+        switch (color)
+        {
+            case 1://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_3_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+            case 2://gold
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_3_Material_Gold");
+                FindObjectOfType<InGameController>().playerCarColor = "yellow";
+                break;
+            case 3://orange
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_3_Material_Orange");
+                FindObjectOfType<InGameController>().playerCarColor = "orange";
+                break;
+            case 4://pink
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_3_Material_Pink");
+                FindObjectOfType<InGameController>().playerCarColor = "magenta";
+                break;
+            case 5://red
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_3_Material_Red");
+                FindObjectOfType<InGameController>().playerCarColor = "red";
+                break;
+            default://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_3_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+        }
+    }
+    //speed color
+    void Car4Color(int color)
+    {
+        switch (color)
+        {
+            case 1://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_4_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+            case 2://gold
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_4_Material_Gold");
+                FindObjectOfType<InGameController>().playerCarColor = "yellow";
+                break;
+            case 3://orange
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_4_Material_Orange");
+                FindObjectOfType<InGameController>().playerCarColor = "orange";
+                break;
+            case 4://pink
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_4_Material_Pink");
+                FindObjectOfType<InGameController>().playerCarColor = "magenta";
+                break;
+            case 5://red
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ShowcaseOnly/Racer_4_Material_Red");
+                FindObjectOfType<InGameController>().playerCarColor = "red";
+                break;
+            default://white
+                car3DObject.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Racer_4_Material_White");
+                FindObjectOfType<InGameController>().playerCarColor = "white";
+                break;
+        }
+    }
     public void MattTest()
     {
         SceneToLoad = "Circle";
         level.SetActive(false);
         level3DObject.SetActive(false);
         car.SetActive(true);
-        //SceneManager.LoadScene("Circle");//rename later maybe
+        car3DObject.SetActive(true);
     }
     public void Butterfly()
     {
@@ -234,7 +446,7 @@ public class MainMenu : MonoBehaviour
         level.SetActive(false);
         level3DObject.SetActive(false);
         car.SetActive(true);
-        //SceneManager.LoadScene("ButterflyTest");//rename later maybe
+        car3DObject.SetActive(true);
     }
     public void Wave()
     {
@@ -242,7 +454,7 @@ public class MainMenu : MonoBehaviour
         level.SetActive(false);
         level3DObject.SetActive(false);
         car.SetActive(true);
-        //SceneManager.LoadScene("WaveTest");//rename later maybe
+        car3DObject.SetActive(true);
     }
     public void Dollar()
     {
@@ -250,30 +462,10 @@ public class MainMenu : MonoBehaviour
         level.SetActive(false);
         level3DObject.SetActive(false);
         car.SetActive(true);
+        car3DObject.SetActive(true);
     }
-    public void CarA(string color)
+    public void StartRace()
     {
-        //save car to gamemanager
-        FindObjectOfType<InGameController>().racerType = 1;
-        FindObjectOfType<InGameController>().playerCarColor = color;
-        SceneManager.LoadScene(SceneToLoad);
-    }
-    public void CarB(string color)
-    {
-        FindObjectOfType<InGameController>().racerType = 2;
-        FindObjectOfType<InGameController>().playerCarColor = color;
-        SceneManager.LoadScene(SceneToLoad);
-    }
-    public void CarC(string color)
-    {
-        FindObjectOfType<InGameController>().racerType = 3;
-        FindObjectOfType<InGameController>().playerCarColor = color;
-        SceneManager.LoadScene(SceneToLoad);
-    }
-    public void CarD(string color)
-    {
-        FindObjectOfType<InGameController>().racerType = 4;
-        FindObjectOfType<InGameController>().playerCarColor = color;
         SceneManager.LoadScene(SceneToLoad);
     }
 }
